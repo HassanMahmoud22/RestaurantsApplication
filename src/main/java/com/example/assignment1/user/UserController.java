dd8a41a1f1e16a69e6f152fa8133b7d509a75ee9
HassanMahmoud22 <hassan444mahmoud@gmail.com>
2022-05-14 21:11:27 +0200

    test

--- src/main/java/com/example/assignment1/user/UserController.java
+++ src/main/java/com/example/assignment1/user/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.sql.SQLException;
 import java.util.Map;
-
+/*test*/
 @CrossOrigin
 @RequestMapping("/api/user")
 @RestController
